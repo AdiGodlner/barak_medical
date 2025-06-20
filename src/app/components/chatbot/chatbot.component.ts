adf1f207b0ecccbb10720e98b5769dea0fc3a97f
Adi Goldner <96goldner@gmail.com>
2025-06-20 19:15:11 +0300

    added simple chatbot functionlaity and stoped need calrification on desgin

--- src/app/components/chatbot/chatbot.component.ts
+++ src/app/components/chatbot/chatbot.component.ts
@@ -1,22 +1,51 @@
 import { Component } from '@angular/core';
-
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { ChatMessage } from '../../models/chat-message.model';
 @Component({
   selector: 'app-chatbot',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './chatbot.component.html',
   styleUrl: './chatbot.component.scss'
 })
 export class ChatbotComponent {
 
-  
   isChatOpen : boolean = false;
-  messages : string[] = [];
+  messages : ChatMessage[] = [];
+  applyForm = new FormGroup({
+    message : new FormControl("")
+  });
 
-
-  toggleChat() : void {
-    
-    this.isChatOpen = !this.isChatOpen;
-
+  close():void {
+    this.isChatOpen = false;
   }
+
+  open():void{
+    this.isChatOpen = true;
+  }
+
+    sendMessage(){
+
+      const user_msg = (this.applyForm.value.message ?? '').trim();
+
+      if(user_msg !== ''){
+        const message : ChatMessage = {
+          from: "user",
+          text: user_msg
+        }
+      
+        this.messages.push(message);
+      
+        const botMessage : ChatMessage = {
+          from: "bot",
+          text: "some text"
+        }
+      
+        this.messages.push(botMessage);
+      
+      }
+      
+      this.applyForm.reset();
+    
+    }
 
 }
