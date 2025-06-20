8faac6f69fc47ceefb10e1df080d213942de8955
Adi Goldner <96goldner@gmail.com>
2025-06-20 23:38:52 +0300

    changed /he/ urls to / urls and added lazy loading of routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,4 @@
 import { Routes } from '@angular/router';
-import { LocationComponent } from './pages/location/location.component';
-import { AboutComponent } from './pages/about/about.component';
 import { HomeComponent } from './pages/home/home.component';
 import { CausalLinkAssessmentComponent } from './pages/services/occupational-medicine/causal-link-assessment/causal-link-assessment.component';
 import { MilitaryMedicineComponent } from './pages/services/additional-services/military-medicine/military-medicine.component';
@@ -10,39 +8,38 @@ import { AviationMedicineComponent } from './pages/services/additional-services/
 import { WorkAbilityComponent } from './pages/services/occupational-medicine/work-ability/work-ability.component';
 import { MedicalRightsComponent } from './pages/services/occupational-medicine/medical-rights/medical-rights.component';
 import { IndustryConsultingComponent } from './pages/services/occupational-medicine/industry-consulting/industry-consulting.component';
-import { AccessibilityComponent } from './pages/accessibility/accessibility.component';
 
 export const routes: Routes = [
     
-    //  TODO add lazy load to routes
+    { path: '', component: HomeComponent},
+    { path: 'about',
+         loadComponent: () => import('./pages/about/about.component').then(m => m.AboutComponent) },
+    { path: 'location',
+         loadComponent:() => import('./pages/location/location.component').then(m => m.LocationComponent) },
+    { path: 'accessibility-statement',
+         loadComponent:() => import('./pages/accessibility/accessibility.component').then( m => m.AccessibilityComponent )},
+    { path: 'services',
+            children:[
+            { path: 'causal-link-assessment',
+                loadComponent:() => import("./pages/services/occupational-medicine/causal-link-assessment/causal-link-assessment.component").then(m => m.CausalLinkAssessmentComponent)},
+            { path: 'industry-consulting',
+                loadComponent:() => import("./pages/services/occupational-medicine/industry-consulting/industry-consulting.component").then(m => m.IndustryConsultingComponent)},
+            { path: 'medical-rights',
+                loadComponent:() => import("./pages/services/occupational-medicine/medical-rights/medical-rights.component").then(m => m.MedicalRightsComponent)},
+            { path: 'work-ability',
+                loadComponent:() => import("./pages/services/occupational-medicine/work-ability/work-ability.component").then(m => m.WorkAbilityComponent)},
 
-    { path: '', component: HomeComponent },
-    {
-        path: 'he',
-        children:[
-            
-            { path: 'about', component: AboutComponent },
-            { path: 'location', component: LocationComponent },
-            { path: 'accessibility-statement', component:AccessibilityComponent },
-            { path: 'services',
-                 children:[
-                    { path: 'causal-link-assessment', component: CausalLinkAssessmentComponent },
-                    { path: 'industry-consulting', component: IndustryConsultingComponent },
-                    { path: 'medical-rights', component: MedicalRightsComponent },
-                    { path: 'work-ability', component: WorkAbilityComponent },
+            { path: 'aviation-medicine',
+                loadComponent:() => import("./pages/services/additional-services/aviation-medicine/aviation-medicine.component").then(m => m.AviationMedicineComponent)},
+            { path: 'diving-medicine',
+                loadComponent:() => import("./pages/services/additional-services/diving-medicine/diving-medicine.component").then(m => m.DivingMedicineComponent)},
+            { path: 'medical-administration',
+                loadComponent:() => import("./pages/services/additional-services/medical-administration/medical-administration.component").then(m => m.MedicalAdministrationComponent)},
+            { path: 'military-medicine',
+                loadComponent:() => import("./pages/services/additional-services/military-medicine/military-medicine.component").then(m => m.MilitaryMedicineComponent)},
 
-                    { path: 'aviation-medicine', component: AviationMedicineComponent },
-                    { path: 'diving-medicine', component: DivingMedicineComponent },
-                    { path: 'medical-administration', component: MedicalAdministrationComponent },
-                    { path: 'military-medicine', component: MilitaryMedicineComponent },
-
-                 ] 
-                },
-            
-
-        ]
-
-    },
+            ] 
+        },
     
     { path: '**', redirectTo: '' }, // catch-all reroute to home
 
