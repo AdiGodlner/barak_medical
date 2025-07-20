c844945f464f20ee5b7b771c77879d0bf0c12afd
Adi Goldner <96goldner@gmail.com>
2025-07-20 14:08:17 +0300

    changed background of button in nav

--- src/app/models/chat-message.model.ts
+++ src/app/models/chat-message.model.ts
@@ -4,3 +4,38 @@ export interface ChatMessage {
     text : string;
 
 }
+
+type NodeType = 'question' | 'answer';
+
+export interface ChatNodeBase {
+
+  id: string;
+  type: NodeType;
+
+}
+
+export interface AnswerNode extends ChatNodeBase {
+
+    type: 'answer';
+  text: string;
+
+}
+
+export interface nodeButton  {
+
+    label: string;
+    next: string; // reference to another node by ID
+
+}
+
+
+export interface QuestionNode extends ChatNodeBase {
+
+    type: 'question';
+    question: string;
+    options: nodeButton [];
+
+}
+
+
+export type ChatNode = QuestionNode | AnswerNode;
