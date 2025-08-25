2947c0ddba39c1d593dc4779cad2edfc100a0543
Adi Goldner <96goldner@gmail.com>
2025-08-25 22:35:38 +0300

    changing some text light css changes added internal links that may be removed later

--- src/app/pages/home/who-we-are/who-we-are.component.ts
+++ src/app/pages/home/who-we-are/who-we-are.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-who-we-are',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './who-we-are.component.html',
   styleUrl: './who-we-are.component.scss'
 })
