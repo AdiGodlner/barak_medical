85669ba23479694b9b33337c4cf1db5f502ab082
Adi Goldner <96goldner@gmail.com>
2025-10-01 11:36:30 +0300

    trying some new sizes for cards again

---
+++ src/app/components/about-dr/about-dr.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-dr',
+  imports: [],
+  templateUrl: './about-dr.component.html',
+  styleUrl: './about-dr.component.scss'
+})
+export class AboutDrComponent {
+
+}
