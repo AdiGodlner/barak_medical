955589f37241eb78c6074b57947d007c5e49d6c2
Adi Goldner <96goldner@gmail.com>
2025-10-27 22:05:28 +0200

    added basic email form layout

--- src/app/pages/location/location.component.ts
+++ src/app/pages/location/location.component.ts
@@ -1,36 +1,69 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { SeoService } from '../../services/seo.service';
 import { SeoData } from '../../models/seo.model';
-// import { GoogleMapsWraperComponent } from '../../components/google-maps-wraper/google-maps-wraper.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-location',
-  imports: [ 
-    // GoogleMapsWraperComponent
-  ],
+  imports: [ReactiveFormsModule  ],
   templateUrl: './location.component.html',
   styleUrl: './location.component.scss'
 })
+
 export default class LocationComponent implements OnInit{
 
+  
+  private fb = inject(FormBuilder);
+  private seo = inject(SeoService);
+  submitted = signal(false);
 
-    constructor(private seo: SeoService){
-      
+  contactForm = this.fb.group({
+    fullName: [
+      '',
+      [Validators.required, Validators.pattern(/^[A-Za-z\s]+$/)]
+    ],
+    phone: [
+      '',
+      [Validators.required, Validators.pattern(/^[0-9]{9,10}$/)]
+    ],
+    email: [
+      '',
+      [Validators.required, Validators.email]
+    ],
+    message: [
+      '',
+      [Validators.required, Validators.minLength(10)]
+    ]
+  });
+
+  
+  ngOnInit(): void {
+  
+    const pageSeo : SeoData ={
+      title: "צור קשר",
+      description: "צור קשר",
+    };
+
+    this.seo.updateSeoPageData(pageSeo);
+
+  }
+
+  onSubmit() {
+    
+    if (this.contactForm.valid) {
+      console.log('Form submitted:', this.contactForm.value);
+      // TODO: send to backend or service
+      alert('Your message has been sent!');
+      this.contactForm.reset();
+    } else {
+      this.contactForm.markAllAsTouched();
     }
-  
-  
-    ngOnInit(): void {
-  
-      const pageSeo : SeoData ={
-        title: "מיקומינו",
-        description: "מיקומינו",
-      };
-  
-      this.seo.updateSeoPageData(pageSeo);
-  
-    }
+  }
 
-
+  get f() {
+    return this.contactForm.controls;
+  }
 
     
 
