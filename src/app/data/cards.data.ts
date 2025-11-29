ac9c4c6fe0c147930f4180447218f99a7df00aa5
Adi Goldner <96goldner@gmail.com>
2025-11-29 21:20:53 +0200

    changeing images for cards

--- src/app/data/cards.data.ts
+++ src/app/data/cards.data.ts
@@ -1,56 +1,61 @@
 import { Card } from "../models/cards";
 
+
+           
+     
+             
+
 export const militaryMedicine: Card = {
-  imageUrl: '/assets/images/cards/military-med.svg',
+  imageUrl: '/assets/images/cards/refuha zaba.svg',
   title: 'רפואה צבאית',
   description: 'ניסיון רב באבחון רפואי, ייעוץ מקצועי וכתיבת חוות דעת רפואיות לחיילים ולמערכות צבאיות, בהתאם לנהלי הרפואה הצבאית ותקנות צה"ל.',
   routeTo: '/services/military-medicine'
 };
 
 export const medicalAdministration: Card = {
-  imageUrl: '/assets/images/cards/med-admin.svg',
+  imageUrl: '/assets/images/cards/minal refui.svg',
   title: 'מנהל רפואי',
   description: 'ייעוץ וליווי מקצועי למערכות בריאות וארגונים בבניית נהלים רפואיים, תקנים מקצועיים ותהליכי קבלת החלטות רפואיות ותעסוקתיות.',
   routeTo: '/services/medical-administration'
 };
 
 export const divingMedicine: Card = {
-  imageUrl: '/assets/images/placeholder.jpg',
+  imageUrl: '/assets/images/cards/refuha zlila.svg',
   title: 'רפואת צלילה',
   description: 'הערכות כשירות לצלילה, אבחון מצבים רפואיים הקשורים לעולם התת ימי, ומתן ייעוץ רפואי לצוללים ולמועדוני צלילה.',
   routeTo: '/services/diving-medicine'
 };
 
 export const aviationMedicine: Card = {
-  imageUrl: '/assets/images/placeholder.jpg',
+  imageUrl: '/assets/images/cards/refua teufa.svg',
   title: 'רפואה תעופתית',
   description: 'ביצוע בדיקות רפואיות לטייסים, רופאים ואנשי צוות אוויר, ומתן חוות דעת תעופתיות בהתאם לסטנדרטים רפואיים בינלאומיים.',
   routeTo: '/services/aviation-medicine'
 };
 
 export const workAbility: Card = {
-  imageUrl: '/assets/images/cards/work-ability.svg',
+  imageUrl: '/assets/images/cards/kusher havoda.svg',
   title: 'כושר עבודה',
   description: ' הערכת כשירות רפואית לעבודה, התאמת עובדים לתפקידם ומתן חוות דעת רפואיות מקצועיות בהתאם לדרישות מקום העבודה ולרגולציה.',
   routeTo: '/services/work-ability'
 };
 
 export const industryConsulting: Card = {
-  imageUrl: '/assets/images/placeholder.jpg',
+  imageUrl: '/assets/images/cards/yeuth legufim gdulim.svg',
   title: 'ייעוץ לארגונים',
   description: ' ליווי רפואי לארגונים ולחברות בתהליכי קבלת החלטות תעסוקתיות, רפואיות וניהול סיכונים בתחום בריאות העובדים.',
   routeTo: '/services/industry-consulting'
 };
 
 export const causalLinkAssessment: Card = {
-  imageUrl: '/assets/images/cards/cause.svg',
+  imageUrl: '/assets/images/cards/kviat kesher sibati.svg',
   title: 'קשר סיבתי',
   description: 'חוות דעת מומחיות לקביעת הקשר שבין המצב הרפואי לבין תנאי העבודה, מחלות מקצוע או אירועים חריגים.',
   routeTo: '/services/causal-link-assessment'
 };
 
 export const medicalRights: Card = {
-  imageUrl: '/assets/images/placeholder.jpg',
+  imageUrl: '/assets/images/cards/mitzoi zhuout refuieot.svg',
   title: 'מיצוי זכויות',
   description: 'סיוע מקצועי בהצגת חוות דעת רפואיות מול ביטוח לאומי, חברות ביטוח ובתי משפט, לצורך מיצוי זכויות רפואיות ותעסוקתיות.',
   routeTo: '/services/medical-rights'
